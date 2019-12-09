589b4027a82ef15a019b49519e2666db5c733bf0
Abdul Majeed Alkattan <abedelmajeedk@gmail.com>
2019-12-09 14:23:37 +0100

    changed license

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup
+import setuptools
 
 setup(
     name='searchads_api',
-    packages=['searchads_api'],
-    description='Searchads API python library',
+    description='Apple Searchads API non-official python library',
     version='0.1',
-    url='https://github.com/alkattan/searchads_api',
+    url='https://github.com/phiture/searchads_api',
     author='Abdul Majeed Alkattan',
     author_email='alkattan@phiture.com',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7.4",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     keywords=['python','searchads','library'],
     install_requires=['requests>=2.22.0']
     )
